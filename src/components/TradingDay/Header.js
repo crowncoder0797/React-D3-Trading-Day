17aa9dc5f86445067d39d2ca32017fd9164b56d3
fullstackdeveloper1206 <bluestar90116@gmail.com>
2019-07-10 12:26:13 +0800

    header redone

--- src/components/TradingDay/Header.js
+++ src/components/TradingDay/Header.js
@@ -15,6 +15,73 @@ import { DataContext } from "./MarketData";
 import Clock from "react-live-clock";
 import SearchInput from "./Search/index.js";
 
+const HeaderStyles = styled.div`
+  user-select: none;
+  @import url(https://fonts.googleapis.com/css?family=Arvo:700);
+  @import url(https://fonts.googleapis.com/css?family=Seaweed+Script);
+  #title_intro {
+    background-color: #222;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    padding: 20px 0;
+  }
+  .tradingDay {
+    font-size: 5rem;
+    color: #fff;
+    font-family: Arvo;
+    font-weight: bold;
+    text-shadow: -3px -3px 0 #222, 3px -3px 0 #222, -3px 3px 0 #222,
+      3px 3px 0 #222, 4px 4px 0 #fff, 5px 5px 0 #fff, 6px 6px 0 #fff,
+      7px 7px 0 #fff;
+    letter-spacing: 0.1em;
+    margin: 0;
+    margin-top: -0.2em;
+    margin-bottom: 0.2em;
+    text-align: center;
+    line-height: 1em;
+  }
+
+  .lined span {
+    background-color: #222;
+    padding: 0 0.3em;
+    display: inline-block;
+    position: relative;
+    font-family: "Seaweed Script";
+    color: #fff;
+    text-align: center;
+    font-size: 2.5rem;
+    margin: 0;
+    line-height: 1em;
+  }
+  .lined span:before,
+  .lined span:after {
+    content: "";
+    position: absolute;
+    height: 0.5em;
+    border-bottom: 10px solid white;
+    top: 0;
+    width: 100vw;
+  }
+  .lined span:before {
+    right: 100%;
+    margin-right: 15px;
+  }
+  .lined span:after {
+    left: 100%;
+    margin-left: 15px;
+  }
+
+  .clock {
+  }
+
+  .search {
+  }
+`;
+
+
 const StyleWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -64,7 +131,7 @@ export default props => {
   const handleSidebarHide = () => setVisible(false);
   return (
     <>
-      <StyleWrapper>
+      {/* <StyleWrapper>
         <Segment center inverted color='black'>
           <Header textAlign='center'>
             <Header.Subheader className='titleTop'>
@@ -101,7 +168,7 @@ export default props => {
             </Header.Subheader>
             <div style={{ display: "flex", flexDirection: "column" }}>
               <Header.Content className='title' size='huge'>
-                {/* <img src='./../../assets/orange-wheel-art-800x800.png' style={{left:0, height:50, width:50}}></img> */}
+                
                 <span className='tradingdayTD'>T</span>
                 rading
                 <span className='tradingdayTD'>D</span>
@@ -113,8 +180,51 @@ export default props => {
             </div>
           </Header>
         </Segment>
-      </StyleWrapper>
-     
+      </StyleWrapper> */}
+      <HeaderStyles className='App'>
+        <div
+          style={{
+       
+            textAlign: "center",
+            background: "white",
+            color: "#000",
+            letterSpacing: "0.5rem",
+            color: "black",
+            fontWeight: "500",
+            fontSize: "1.5rem",
+            zIndex: "100",
+            borderRadius: "0.25em",
+            lineHeight: "2rem",
+            alignSelf: "center"
+          }}
+          className='clock'>
+          <Clock
+            format={"HH:mm:ss"}
+            ticking={true}
+            timezone={"America/New_York"}
+            interval={1000}
+          />
+        </div>
+        <div id='title_intro'>
+          <p className='lined'>
+            <span>The American Financial Markets</span>
+          </p>
+          <p className='tradingDay'>
+            Trading Day
+          </p>
+          <p className='lined'>
+            <span>by Ajay Phogat</span>
+          </p>
+          <div
+            className='search'
+            style={{
+              width: "100%"
+            }}>
+            <SearchInput />
+          </div>
+        </div>
+      </HeaderStyles>
+
       {/* <ScrollBasedBezier headerHeight={100} /> */}
     </>
   );
