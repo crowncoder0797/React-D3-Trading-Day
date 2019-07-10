c183bb914d89aeb1f24ee4337b6d3952e68f850d
fullstackdeveloper1206 <bluestar90116@gmail.com>
2019-07-10 10:34:38 +0800

    builds

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-// import Home, { ObservableNotebook } from "./Home";
+import Home from "./Home";
 //TRADING-DAY
 //---------------------------------------------------
 //ROUTES
